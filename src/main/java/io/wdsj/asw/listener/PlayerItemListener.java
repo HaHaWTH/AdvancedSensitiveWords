b30a5d1e765115c86f35a7983e283e132d3bbe42
HaHaWTH <id_cn00@outlook.com>
2024-03-17 03:13:24 +0800

    Add cache feature

--- src/main/java/io/wdsj/asw/listener/PlayerItemListener.java
+++ src/main/java/io/wdsj/asw/listener/PlayerItemListener.java
@@ -19,12 +19,14 @@ import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.HashSet;
 import java.util.List;
 
 import static io.wdsj.asw.AdvancedSensitiveWords.*;
 import static io.wdsj.asw.util.Utils.messagesFilteredNum;
 
 public class PlayerItemListener implements Listener {
+    private static final HashSet<ItemMeta> NORMAL_ITEM_META = new HashSet<>();
 
     @EventHandler(priority = EventPriority.LOW)
     public void onPlayerHeldItem(PlayerItemHeldEvent event) {
@@ -34,6 +36,7 @@ public class PlayerItemListener implements Listener {
         ItemStack item = player.getInventory().getItem(event.getNewSlot());
         if (item != null && item.hasItemMeta()) {
             ItemMeta meta = item.getItemMeta();
+            if (NORMAL_ITEM_META.contains(meta)) return;
             if (meta != null && meta.hasDisplayName()) {
                 String originalName = meta.getDisplayName();
                 long startTime = System.currentTimeMillis();
@@ -61,6 +64,8 @@ public class PlayerItemListener implements Listener {
                     TimingUtils.addProcessStatistic(endTime, startTime);
                     if (settingsManager.getProperty(PluginSettings.NOTICE_OPERATOR)) Notifier.notice(player, EventType.ITEM, originalName);
                     if (settingsManager.getProperty(PluginSettings.ITEM_PUNISH)) Punishment.punish(player);
+                } else {
+                    NORMAL_ITEM_META.add(meta);
                 }
             }
         }
@@ -74,6 +79,7 @@ public class PlayerItemListener implements Listener {
         ItemStack item = event.getItemDrop().getItemStack();
         if (item.hasItemMeta()) {
             ItemMeta meta = item.getItemMeta();
+            if (NORMAL_ITEM_META.contains(meta)) return;
             if (meta != null && meta.hasDisplayName()) {
                 String originalName = meta.getDisplayName();
                 long startTime = System.currentTimeMillis();
@@ -101,6 +107,8 @@ public class PlayerItemListener implements Listener {
                     TimingUtils.addProcessStatistic(endTime, startTime);
                     if (settingsManager.getProperty(PluginSettings.NOTICE_OPERATOR)) Notifier.notice(player, EventType.ITEM, originalName);
                     if (settingsManager.getProperty(PluginSettings.ITEM_PUNISH)) Punishment.punish(player);
+                } else {
+                    NORMAL_ITEM_META.add(meta);
                 }
             }
         }
