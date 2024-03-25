b823ee5e486a1039bdfe3a743e3af81316b03076
HaHaWTH <id_cn00@outlook.com>
2024-03-25 12:27:31 +0800

    fix

--- src/main/java/io/wdsj/asw/command/ConstructTabCompleter.java
+++ src/main/java/io/wdsj/asw/command/ConstructTabCompleter.java
@@ -7,7 +7,6 @@ import org.bukkit.command.TabCompleter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collections;
 import java.util.List;
 
 public class ConstructTabCompleter implements TabCompleter {
