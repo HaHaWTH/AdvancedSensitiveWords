ad8c37fb0b0f53dbe64ce4135d3b399389ca1824
HaHaWTH <id_cn00@outlook.com>
2024-04-17 23:51:12 +0800

    BungeeCord init

---
+++ bungee/src/main/java/io/wdsj/asw/bungee/AdvancedSensitiveWords.java
@@ -0,0 +1,16 @@
+package io.wdsj.asw.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class AdvancedSensitiveWords extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
