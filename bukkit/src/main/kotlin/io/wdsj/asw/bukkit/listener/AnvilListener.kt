53737f4f54a1ebcbeafb967976ceb199fd076871
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2024-06-16 00:41:53 +0800

    AnvilListener

---
+++ bukkit/src/main/kotlin/io/wdsj/asw/bukkit/listener/AnvilListener.kt
@@ -0,0 +1,115 @@
+package io.wdsj.asw.bukkit.listener
+
+import io.wdsj.asw.bukkit.AdvancedSensitiveWords
+import io.wdsj.asw.bukkit.event.ASWFilterEvent
+import io.wdsj.asw.bukkit.event.EventType
+import io.wdsj.asw.bukkit.manage.notice.Notifier
+import io.wdsj.asw.bukkit.manage.permission.Permissions
+import io.wdsj.asw.bukkit.manage.punish.Punishment
+import io.wdsj.asw.bukkit.proxy.bungee.BungeeSender
+import io.wdsj.asw.bukkit.proxy.velocity.VelocitySender
+import io.wdsj.asw.bukkit.setting.PluginMessages
+import io.wdsj.asw.bukkit.setting.PluginSettings
+import io.wdsj.asw.bukkit.util.TimingUtils
+import io.wdsj.asw.bukkit.util.Utils
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryType
+
+class AnvilListener : Listener {
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    fun onAnvil(event: InventoryClickEvent) {
+        if (!AdvancedSensitiveWords.isInitialized) return
+        if (!AdvancedSensitiveWords.settingsManager.getProperty(PluginSettings.ENABLE_ANVIL_EDIT_CHECK)) return
+        if (event.inventory.type == InventoryType.ANVIL) {
+            val player = event.whoClicked as Player
+            if (player.hasPermission(Permissions.BYPASS)) {
+                return
+            }
+            if (event.rawSlot == 2) {
+                val outputItem = event.currentItem
+                val isCancelMode = AdvancedSensitiveWords.settingsManager.getProperty(PluginSettings.ANVIL_METHOD).equals("cancel", ignoreCase = true)
+                if (outputItem != null && outputItem.hasItemMeta()) {
+                    val itemMeta = outputItem.itemMeta
+                    if (itemMeta != null && itemMeta.hasDisplayName()) {
+                        var originalItemName = itemMeta.displayName
+                        if (AdvancedSensitiveWords.settingsManager.getProperty(PluginSettings.PRE_PROCESS)) originalItemName =
+                            originalItemName.replace(
+                                Utils.getPreProcessRegex().toRegex(), ""
+                            )
+                        val censoredWords = AdvancedSensitiveWords.sensitiveWordBs.findAll(originalItemName)
+                        if (censoredWords.isNotEmpty()) {
+                            val startTime = System.currentTimeMillis()
+                            Utils.messagesFilteredNum.getAndIncrement()
+                            val processedItemName = AdvancedSensitiveWords.sensitiveWordBs.replace(originalItemName)
+                            if (isCancelMode) {
+                                event.isCancelled = true
+                            } else {
+                                itemMeta.setDisplayName(processedItemName)
+                                outputItem.setItemMeta(itemMeta)
+                            }
+
+                            if (AdvancedSensitiveWords.settingsManager.getProperty(PluginSettings.ANVIL_SEND_MESSAGE)) {
+                                player.sendMessage(
+                                    ChatColor.translateAlternateColorCodes(
+                                        '&',
+                                        AdvancedSensitiveWords.messagesManager.getProperty(PluginMessages.MESSAGE_ON_ANVIL_RENAME)
+                                    )
+                                )
+                            }
+
+                            if (AdvancedSensitiveWords.settingsManager.getProperty(PluginSettings.LOG_VIOLATION)) {
+                                Utils.logViolation(
+                                    player.name + "(IP: " + Utils.getPlayerIp(player) + ")(Anvil)",
+                                    originalItemName + censoredWords
+                                )
+                            }
+
+                            if (AdvancedSensitiveWords.settingsManager.getProperty(PluginSettings.ENABLE_API)) {
+                                Bukkit.getPluginManager().callEvent(
+                                    ASWFilterEvent(
+                                        player,
+                                        originalItemName,
+                                        processedItemName,
+                                        censoredWords,
+                                        EventType.ANVIL,
+                                        false
+                                    )
+                                )
+                            }
+
+                            if (AdvancedSensitiveWords.settingsManager.getProperty(PluginSettings.HOOK_VELOCITY)) {
+                                VelocitySender.send(player, EventType.ANVIL, originalItemName, censoredWords)
+                            }
+
+                            if (AdvancedSensitiveWords.settingsManager.getProperty(PluginSettings.HOOK_BUNGEECORD)) {
+                                BungeeSender.send(player, EventType.ANVIL, originalItemName, censoredWords)
+                            }
+
+                            if (AdvancedSensitiveWords.settingsManager.getProperty(PluginSettings.ENABLE_DATABASE)) {
+                                AdvancedSensitiveWords.databaseManager.checkAndUpdatePlayer(player.name)
+                            }
+
+                            val endTime = System.currentTimeMillis()
+                            TimingUtils.addProcessStatistic(endTime, startTime)
+                            if (AdvancedSensitiveWords.settingsManager.getProperty(PluginSettings.NOTICE_OPERATOR)) Notifier.notice(
+                                player,
+                                EventType.ANVIL,
+                                originalItemName,
+                                censoredWords
+                            )
+                            if (AdvancedSensitiveWords.settingsManager.getProperty(PluginSettings.ANVIL_PUNISH)) Punishment.punish(
+                                player
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
