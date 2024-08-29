6d4b888831e6d5a1044a5fa7c9c2ed776dd33ce1
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2024-08-29 19:31:53 +0800

    fix(bukkit): 确保fake消息计数正确自增修复了FakeMessageExecutor.selfIncrement方法，该方法在试图递增fake消息计数时未能正确更新玩家的计数值。现在，计数值将正确增加，防止在尝试递增时出现关联错误。

--- bukkit/src/main/kotlin/io/wdsj/asw/bukkit/listener/FakeMessageExecutor.kt
+++ bukkit/src/main/kotlin/io/wdsj/asw/bukkit/listener/FakeMessageExecutor.kt
@@ -49,7 +49,6 @@ class FakeMessageExecutor : Listener {
 
         @JvmStatic
         fun selfIncrement(player: Player) {
-            FAKE_MESSAGE_NUM.getOrPut(player) { 0 } + 1
-        }
+            FAKE_MESSAGE_NUM[player] = FAKE_MESSAGE_NUM.getOrPut(player) { 0 } + 1        }
     }
 }
